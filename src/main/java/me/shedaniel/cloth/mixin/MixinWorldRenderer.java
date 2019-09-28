0d30e50b5fb360313ce65658a89f75ce67f3d02e
Danielshe <shekwancheung0528@gmail.com>
2019-09-28 14:56:39 +0800

    Fix mixin

--- src/main/java/me/shedaniel/cloth/mixin/MixinWorldRenderer.java
+++ src/main/java/me/shedaniel/cloth/mixin/MixinWorldRenderer.java
@@ -1,6 +1,9 @@
 package me.shedaniel.cloth.mixin;
 
 import me.shedaniel.cloth.hooks.ClothClientHooks;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +16,7 @@ public class MixinWorldRenderer {
     @Inject(method = "method_22710",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/LightmapTextureManager;enable()V",
                      ordinal = 1, shift = At.Shift.BEFORE))
-    public void renderCenter(float delta, long long_1, CallbackInfo callbackInfo) {
+    public void renderCenter(float float_1, long long_1, boolean boolean_1, Camera camera_1, GameRenderer gameRenderer_1, LightmapTextureManager lightmapTextureManager_1, CallbackInfo callbackInfo) {
         ClothClientHooks.DEBUG_RENDER_PRE.invoker().run();
     }
     
