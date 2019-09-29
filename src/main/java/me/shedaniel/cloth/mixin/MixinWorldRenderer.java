3bc640782ef28de5a72d39fd1a88bac35f613cdb
Danielshe <shekwancheung0528@gmail.com>
2019-09-29 15:41:54 +0800

    Update to 19w39a

--- src/main/java/me/shedaniel/cloth/mixin/MixinWorldRenderer.java
+++ src/main/java/me/shedaniel/cloth/mixin/MixinWorldRenderer.java
@@ -1,6 +1,7 @@
 package me.shedaniel.cloth.mixin;
 
 import me.shedaniel.cloth.hooks.ClothClientHooks;
+import net.minecraft.class_4587;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
@@ -14,9 +15,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinWorldRenderer {
     
     @Inject(method = "method_22710",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/LightmapTextureManager;enable()V",
-                     ordinal = 1, shift = At.Shift.BEFORE))
-    public void renderCenter(float float_1, long long_1, boolean boolean_1, Camera camera_1, GameRenderer gameRenderer_1, LightmapTextureManager lightmapTextureManager_1, CallbackInfo callbackInfo) {
+            at = @At(value = "INVOKE",
+                     target = "Lnet/minecraft/class_4597$class_4598;method_22994(Lnet/minecraft/block/BlockRenderLayer;)V",
+                     ordinal = 0, shift = At.Shift.BEFORE))
+    public void renderCenter(class_4587 class_4587_1, float float_1, long long_1, boolean boolean_1, Camera camera_1, GameRenderer gameRenderer_1, LightmapTextureManager lightmapTextureManager_1, CallbackInfo callbackInfo) {
         ClothClientHooks.DEBUG_RENDER_PRE.invoker().run();
     }
     
