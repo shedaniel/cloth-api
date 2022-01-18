36eff0ee9300d0104b846c3d06e31c150d7149c8
shedaniel <daniel@shedaniel.me>
2022-01-19 00:12:07 +0800

    Fix #41
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- cloth-client-events-v0/src/main/java/me/shedaniel/cloth/mixin/client/events/MixinKeyboard.java
+++ cloth-client-events-v0/src/main/java/me/shedaniel/cloth/mixin/client/events/MixinKeyboard.java
@@ -95,7 +95,7 @@ public class MixinKeyboard {
                                        target = "Lnet/minecraft/client/gui/screen/Screen;wrapScreenError(Ljava/lang/Runnable;Ljava/lang/String;Ljava/lang/String;)V",
                                        ordinal = 0, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD,
             cancellable = true)
-    public void onKeyAfter(long long_1, int int_1, int int_2, int int_3, int int_4, CallbackInfo info, Screen screen, boolean bl, boolean[] bls) {
+    public void onKeyAfter(long long_1, int int_1, int int_2, int int_3, int int_4, CallbackInfo info, Screen screen, boolean[] bls) {
         if (!info.isCancelled() && !bls[0]) {
             ActionResult result;
             if (int_3 != 1 && (int_3 != 2 || !this.repeatEvents)) {
