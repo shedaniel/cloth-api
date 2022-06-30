3a91d9e416a427be2d06782a57acdc2ae475059b
shedaniel <daniel@shedaniel.me>
2022-06-30 19:44:10 +0800

    1.19
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- cloth-common-events-v1/src/main/java/me/shedaniel/cloth/mixin/common/events/MixinServerPlayerEntity.java
+++ cloth-common-events-v1/src/main/java/me/shedaniel/cloth/mixin/common/events/MixinServerPlayerEntity.java
@@ -31,11 +31,13 @@ import com.mojang.authlib.GameProfile;
 import me.shedaniel.cloth.api.common.events.v1.PlayerChangeWorldCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.screen.ScreenHandlerListener;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -43,8 +45,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class MixinServerPlayerEntity extends PlayerEntity implements ScreenHandlerListener {
-    public MixinServerPlayerEntity(World world, BlockPos pos, float yaw, GameProfile profile) {
-        super(world, pos, yaw, profile);
+    public MixinServerPlayerEntity(World world, BlockPos pos, float yaw, GameProfile gameProfile, @Nullable PlayerPublicKey publicKey) {
+        super(world, pos, yaw, gameProfile, publicKey);
     }
     
     @Inject(method = "moveToWorld", at = @At("HEAD"))
